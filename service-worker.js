a58a6c74c2e394c50cae5958ce5bdfff4f3658b3
SamX23 <skalexsong@gmail.com>
2020-12-17 20:05:16 +0700

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -37,3 +37,62 @@ self.addEventListener("fetch", function (event) {
       })
   );
 });
+
+const CACHE_NAME = "pwa_cache";
+
+let urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/pages/home.html",
+  "/pages/about.html",
+  "/pages/contact.html",
+  "/css/materialize.min.css",
+  "/js/materialize.min.js",
+  "/js/nav.js",
+];
+
+// save assets to cache
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// load assets from cache
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME })
+      .then(function (response) {
+        if (response) {
+          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+          return response;
+        }
+
+        console.log(
+          "ServiceWorker: Memuat aset dari server: ",
+          event.request.url
+        );
+        return fetch(event.request);
+      })
+  );
+});
+
+// Remove cache on update
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
